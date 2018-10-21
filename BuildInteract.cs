e5a0ac2a244144839da451851c061732e29c7b4c
Cristian Rodriguez <crerodri@ucsc.edu>
2018-10-20 17:21:23 -0700

    Add files via upload
    Added the files for the building interactions because I couldn't get any other code besides the code from the first pull to run

---
+++ BuildInteract.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+public class BuildInteract : MonoBehaviour
+{
+    [SerializeField]
+    Canvas messageCanvas;
+
+    void Start()
+    {
+        messageCanvas.enabled = false;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.name == "player1")
+        {
+            TurnOnMessage();
+        }
+        if (other.name == "player2")
+        {
+            TurnOnMessage();
+        }
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+        }
+    }
+
+    private void TurnOnMessage()
+    {
+        messageCanvas.enabled = true;
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.name == "player1")
+        {
+            TurnOffMessage();
+        }
+        if (other.name == "player2")
+        {
+            TurnOffMessage();
+        }
+    }
+
+    private void TurnOffMessage()
+    {
+        messageCanvas.enabled = false;
+    }
+}
---
+++ PlayerInteraction.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteraction : MonoBehaviour
+{
+    [SerializeField]
+    Canvas messageCanvas;
+    void Start()
+    {
+        messageCanvas.enabled = false;
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name == "player1")
+        {
+            Debug.Log(collision.name);
+            TurnOnMessage();
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.name == "player2")
+        {
+            TurnOffMessage();
+        }
+    }
+
+    private void TurnOnMessage()
+    {
+        messageCanvas.enabled = true;
+    }
+    private void TurnOffMessage()
+    {
+        messageCanvas.enabled = false;
+    }
+}
