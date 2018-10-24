198b3257488ff7974dc0521d2cac567d78f47e1b
jiandact <jiandact@yahoo.com>
2018-10-24 13:35:36 -0700

    Percentbehavior (#2)
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload

---
+++ Assets/Scripts/PercentBehavior.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PercentBehavior : MonoBehaviour {
+
+    private int percent;
+
+	void Update ()
+    {
+        percent = this.transform.parent.GetComponent<PlayerValues>().percentage;
+        string temp= percent.ToString(); ;
+        GetComponent<TextMesh>().text=temp;
+
+	}
+}
