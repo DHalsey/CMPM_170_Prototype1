863cf1401c94ad840397d1253260993a5c9d2295
jiandact <jiandact@yahoo.com>
2018-10-23 23:56:34 -0700

    Add files via upload

---
+++ Assets/Scripts/PercentBehavior.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PercentBehavior : MonoBehaviour {
+
+    private int percent;
+    // Use this for initialization
+    void Start ()
+    {
+        
+        //percent = PlayerValues.GetPercent();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        percent = this.transform.parent.GetComponent<PlayerValues>().percentage;
+        string temp= percent.ToString(); ;
+        GetComponent<TextMesh>().text=temp;
+
+	}
+}
